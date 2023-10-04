28f45afafcf3333d28764a3c3764a70d4c530cf0
githma.a <githma.a@blackvt.com>
2023-10-04 11:36:03 +0530

    Nav bar footer not completed

---
+++ components/common/Form/FormButton.vue
@@ -0,0 +1,52 @@
+<template>
+   
+        <button :id="id" class="flex items-center py-4 text-sm font-bold text-white px-7 bg-purple-blue-500 hover:bg-purple-blue-600 focus:ring-4 focus:ring-purple-blue-100 transition duration-300 rounded-xl">
+        <template v-if="isLoading">
+            <b-spinner small class="mr-2" variant="light"></b-spinner>
+            <span class="text-small text-white">Loading...</span>
+        </template>
+        <template v-else>
+            <slot></slot>
+        </template>
+    </button>
+    </template>
+    
+    <script>
+    /* eslint-disable no-console */
+    export default {
+        name: "FormButton",
+        props: {
+            id: String,
+            type: {
+                default: "button",
+                type: String,
+            },
+            title: {
+                default: "Button",
+                type: String,
+            },
+            size: {
+                default: "md",
+                type: String,
+            },
+         
+            to: {
+                default: null,
+                type: String,
+            },
+           
+            isDisabled: {
+                default: false,
+                type: Boolean,
+            },
+            isLoading: {
+                default: false,
+                type: Boolean,
+            },
+        },
+    };
+    </script>
+    
+    <style scoped>
+   
+    </style>
--- components/common/TopNavBar.vue
+++ components/common/TopNavBar.vue
@@ -1,15 +1,106 @@
 <template>
-<div style="height: 50px;width: 100vw;background-color: black">
-  <h1  style="color: white">Header goes here</h1>
-</div>
+
+<nav class="flex items-center bg-gray-800 p-4 flex-wrap">
+      <a href="#" class="mr-6 ml-3 inline-flex items-center">
+        <img src="@/assets/images/navbarLogo.png" alt="Image Alt Text" class="h-12 w-15 mr-2" />
+       
+      </a>
+      <button
+        class="text-white inline-flex p-3 hover:bg-gray-900 rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler"
+        data-target="#navigation"
+        @click="toggleNav"
+      >
+        <i class="material-icons">menu</i>
+      </button>
+      <div
+        class=" top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto"
+        id="navigation"
+        :class="{ 'block': isNavOpen, 'hidden': !isNavOpen }"
+      >
+        <div
+          class="lg:inline-flex lg:flex-row  lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto"
+        >
+          <a
+            href="#"
+            class="lg:inline-flex lg:w-auto w-full px-5 py-2 rounded text-white items-center justify-center hover:bg-white hover:border hover:border-white hover:bg-opacity-25 hover:text-white"
+          >
+            <span>Services</span>
+          </a>
+          <a
+            href="#"
+            class="lg:inline-flex lg:w-auto w-full px-5 py-2 rounded text-white items-center justify-center hover:bg-white hover:border hover:border-white hover:bg-opacity-25 hover:text-white"
+          >
+            <span>Business Solution</span>
+          </a>
+          <a
+            href="#"
+            class="lg:inline-flex lg:w-auto w-full px-5 py-2 rounded text-white items-center justify-center hover:bg-white hover:border hover:border-white hover:bg-opacity-25 hover:text-white"
+          >
+            <span>Technology</span>
+          </a>
+          <a
+            href="#"
+            class="lg:inline-flex lg:w-auto w-full px-5 py-2 rounded text-white items-center justify-center hover:bg-white hover:border hover:border-white hover:bg-opacity-25 hover:text-white"
+          >
+            <span>Our Company</span>
+          </a>
+          
+          
+        </div>
+        <div
+      class="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto"
+    >
+      <a
+        class="lg:inline-flex lg:w-auto w-full px-5 py-2 rounded text-white items-center justify-center underline"
+        href="#"
+      >
+        <span>Login</span>
+      </a>
+      <a
+        class="lg:inline-flex lg:w-auto w-full px-5 py-2 rounded text-white items-center justify-center"
+        href="#"
+      >
+        <span>Book a Meeting</span>
+      </a>
+    </div>
+      </div>
+    </nav>
 </template>
 
-<script>
+<script >
 export default {
-  name: "TopNavBar"
+  name: "TopNavBar",
+
+  data() {
+    return {
+      isNavOpen: false // Add a data property to track the navigation state
+    };
+  },
+
+  methods: {
+    toggleNav() {
+      console.log("clciked")
+      // Toggle the navigation state when the button is clicked
+      this.isNavOpen = !this.isNavOpen;
+    }
+  }
 }
+
+
 </script>
 
 <style scoped>
+body {
+  font-family: "Rubik", sans-serif;
+}
 
+/* navigation 
+ - show navigation always on the large screen devices with (min-width:1024)
+*/
+
+@media (min-width: 1024px) {
+  .top-navbar {
+    display: inline-flex !important;
+  }
+}
 </style>
