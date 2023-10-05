7aa034fca91d6e5e0a74ba530569916e228d1ad6
githma.a <githma.a@blackvt.com>
2023-10-05 13:24:44 +0530

    change in quotes section

--- pages/index.vue
+++ pages/index.vue
@@ -61,8 +61,8 @@
     <section class="pt-20 lg:max-w-10xl w-full sm:px-10 md:px-12 px-5 lg:px-10 flex flex-col lg:flex-row">
       <div class="container py-10 mx-auto sm:flex sm:justify-center w-full sm:px-10 md:px-12 lg:px-20">
         <div class="flex flex-col text-center w-full items-center ">
-          <h1 class="sm:text-2xl text-4xl xl:text-5xl  title-font mb-4 text-black-200">{{ this.Quote }}</h1>
-          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">{{ this.Speaker }}</p>
+          <h1 class="sm:text-2xl text-4xl xl:text-5xl  title-font mb-4 text-black-200">{{ Quote }}</h1>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">{{ Speaker }}</p>
         </div>
       </div>
     </section>
