b19a8d5a5dbafb09933c2ef6874063d036fe0f89
githma.a <githma.a@blackvt.com>
2023-10-05 16:17:27 +0530

    Form input field changes

--- components/common/Form/FormLargeTextBox.vue
+++ components/common/Form/FormLargeTextBox.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- <div class=" mb-3" > -->
-        <textarea  id="id" rows="4" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm  placeholder:text-gray-400  sm:text-sm sm:leading-6 h-40 focus:outline-none" ></textarea>
+        <textarea  id="id" rows="rows" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm  placeholder:text-gray-400  sm:text-sm sm:leading-6 h-40 focus:outline-none" ></textarea>
   </template>
   
   <script>
@@ -18,10 +18,7 @@
           type: String,
         },
         isRequired: Boolean,
-        trim: {
-          default: false,
-          type: Boolean,
-        },
+        
         placeholder: String,
         invalidFeedback: {
           default: "",
@@ -35,25 +32,7 @@
           default: false,
           type: Boolean,
         },
-        state: {
-          default: null,
-          type: Boolean,
-        },
-        form: {
-          default: "",
-          type: String,
-        },
-        whiteBG: {
-          default: true,
-          type: Boolean,
-        },
-        value: {
-          default: null,
-        },
-        counter: {
-          default: false,
-          type: [Number, Boolean],
-        },
+       
       },
   };
   </script>
--- components/common/Form/FormSelectField.vue
+++ components/common/Form/FormSelectField.vue
@@ -1,8 +1,7 @@
 <template>
     <select id="id" name="name" autocomplete="autocomplete" class=" mb-3 py-2 px-3  focus:border-green-200 focus:outline-none block w-full rounded-md border-0   text-black-100 shadow-sm ">
-                <option>United States</option>
-                <option>Canada</option>
-                <option>Mexico</option>
+                <option>Request demonstration</option>
+                
               </select>
 </template>
 <script>
@@ -15,47 +14,14 @@ export default {
       name: String,
       autocomplete: String,
       type: String,
-      description: {
-        default: "",
-        type: String,
-      },
+      
       isRequired: Boolean,
-      trim: {
-        default: false,
-        type: Boolean,
-      },
+      
       placeholder: String,
-      invalidFeedback: {
-        default: "",
-        type: String,
-      },
-      isTextArea: {
-        default: false,
-        type: Boolean,
-      },
-      isDisabled: {
-        default: false,
-        type: Boolean,
-      },
-      state: {
-        default: null,
-        type: Boolean,
-      },
-      form: {
-        default: "",
-        type: String,
-      },
-      whiteBG: {
-        default: true,
-        type: Boolean,
-      },
-      value: {
-        default: null,
-      },
-      counter: {
-        default: false,
-        type: [Number, Boolean],
-      },
+      
+     
+        options: ["Request demonstration", "Request demonstration"],
+      
     },
 };
 </script>
