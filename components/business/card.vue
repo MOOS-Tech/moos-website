b628f97186bb7f01f3898a78c6feb313dd10cfe4
Dhanushka Sandaruwan <dhanushkasandaruwan350@gmail.com>
2023-11-07 12:48:19 +0530

    fix: image width issue

--- components/business/card.vue
+++ components/business/card.vue
@@ -1,12 +1,12 @@
 <template>
-  
+
     <section class="relative  bg-white">
       <div class="w-full flex flex-col lg:flex-row">
       <div
           class="mx-auto lg:max-w-4xl w-full pt-10 lg:pt-20 sm:px-10 md:px-12 lg:px-30 flex flex-col lg:flex-row"
       >
         <div
-            class="relative flex flex-col items-center text-left lg:text-left lg:py-7 xl:py-8 
+            class="relative flex flex-col items-center text-left lg:text-left lg:py-7 xl:py-8
         lg:items-start lg:max-w-none max-w-3xl mx-auto lg:mx-0  lg:w-2/3"
         >
           <CommonTitle v-if="ComTitle" :CardTitle="ComTitle" :BoldText="boldText"/>
@@ -21,21 +21,21 @@
           </ul>
         </div>
 
-        
+
       </div>
       <div
             class="flex  lg:w-1/4 lg:h-auto relative lg:max-w-none lg:mx-0 mx-auto max-w-3xl justify-end "
         >
           <img
-              :src="baseUrl + imageUrl"
+              :src="imageUrl.includes('http')?imageUrl:baseUrl + imageUrl"
               alt="Hero image"
-             
-              class="lg:absolute lg:w-full lg:h-full rounded-xl object-cover lg:max-h-none max-h-[550px] max-w-[250px]"
+
+              class="lg:absolute lg:w-full lg:h-full object-cover  lg:max-w-[400px] min-h-[300px]"
           />
         </div>
       </div>
     </section>
-  
+
 </template>
 
 
