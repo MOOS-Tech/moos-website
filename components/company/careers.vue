aaad51a79ac3713dd17a059cb49cc8a76e126533
githma.a <githma.a@blackvt.com>
2023-11-08 13:38:30 +0530

    file upload careers page

--- components/company/careers.vue
+++ components/company/careers.vue
@@ -1,6 +1,7 @@
 <template>
-  <section class="flex mb-16  justify-center bg-green-and-black" data-aos="fade-up" data-aos-duration="1000">
-    <div class=" mx-auto w-full lg:max-w-7xl ">
+  <section class="relative flex mb-16  justify-center " data-aos="fade-up" data-aos-duration="1000">
+    <div class="bg-image"></div>
+    <div class=" mx-auto w-full lg:max-w-7xl relative ">
       <div class="grid md:grid-cols-2 md:gap-12 lg:gap-48 pt-12 pb-12">
 
         <div class="flex justify-center md:justify-end w-full self-center">
@@ -10,25 +11,23 @@
                 <h2 class="text-regular-title-heading font-semibold">{{ card.title }}</h2>
                 <ul>
                   <li v-for="description in card.qualifications" :key="description.id" class="sm:text-sm">
-                                        <span class="text-green-500 mr-2">
-                                            <i class="fas fa-check"></i>
-                                        </span>
+                    <span class="text-green-500 mr-2">
+                      <i class="fas fa-check"></i>
+                    </span>
                     {{ description }}
                   </li>
                 </ul>
                 <div class="flex gap-2">
                   <div>
-                    <button class="bg-green-200 text-white px-4 py-1 rounded-md mt-4" @click="openLink(card.link)">View Job</button>
+                    <button class="bg-green-200 text-white px-4 py-1 rounded-md mt-4" @click="openLink(card.link)">View
+                      Job</button>
                   </div>
                   <div>
                     <button class="bg-green-200 text-white px-4 py-1 rounded-md mt-4"
-                            @click="setSelectedPosition(card.title)">Apply
+                      @click="setSelectedPosition(card.title)">Apply
                     </button>
                   </div>
-
                 </div>
-
-
               </div>
             </div>
           </div>
@@ -38,12 +37,22 @@
             <h1 class="text-normal-title-heading font-bold text-black-200 ">Join with us</h1>
           </div>
           <div class="flex flex-col items-center justify-center">
-            <FormSelectField :name="selectedPosition" placeholder="Position" :options="options"/>
-            <FormInput v-model="customerName" type="text" name="name" id="name" placeholder="Name"/>
-            <FormInput v-model="customerEmail" type="email" name="email" id="email" placeholder="Email"/>
+            <FormSelectField :name="selectedPosition" placeholder="Position" :options="options" />
+            <FormInput v-model="customerName" type="text" name="name" id="name" placeholder="Name" />
+            <FormInput v-model="customerEmail" type="email" name="email" id="email" placeholder="Email" />
             <FormInput v-model="customerLinkedin" type="text" name="linkedin" id="linkedin"
-                       placeholder="LinkedIn Profile"/>
-            <FormInput v-model="customerResume" name="Resume" id="Resume" placeholder="Upload Resume"/>
+              placeholder="LinkedIn Profile" />
+            <!-- <FormInput v-model="customerResume" name="Resume" id="Resume" placeholder="Upload Resume" /> -->
+            <div class="mb-3 relative">
+              <input
+                class="rounded-[4px] border p-3 pr-10 hover:outline-none focus:outline-none hover:border-green-200 h-10 w-72 sm:text-sm"
+                placeholder="Upload Resume"
+                :value="selectedFileName" />
+              <label class="absolute right-2 top-2 cursor-pointer">
+                <i class="fas fa-paperclip text-gray-300"></i>
+                <input id="file" type="file" class="hidden" accept=".pdf" @change="uploadFile" ref="fileInput" />
+              </label>
+            </div>
             <FormButton class="text-white" @click="Submitfn">Submit</FormButton>
           </div>
         </section>
@@ -56,7 +65,7 @@ import FormInput from "@/components/common/Form/FormInputField";
 import FormButton from "@/components/common/Form/FormButton";
 import FormSelectField from "@/components/common/Form/FormSelectField";
 import FormLargeTextBox from "@/components/common/Form/FormLargeTextBox";
-import {joinWithUs, getCareerPositions} from "@/services/about.js";
+import { joinWithUs, getCareerPositions } from "@/services/about.js";
 
 export default {
   name: "careers",
@@ -80,27 +89,46 @@ export default {
       cardData: [],
       cards: [],
       options: [],
-      selectedPosition: ''
+      selectedPosition: '',
+      selectedFile: null,
+      selectedFileName :""
     }
   },
   async created() {
     await this.fetchCareerPositions();
   },
   methods: {
-    openLink(val){
+    openLink(val) {
       window.open(val, '_blank');
     },
     setSelectedPosition(val) {
       this.selectedPosition = val;
     },
+    openFileInput() {
+      this.$refs.fileInput.click();
+    },
+    uploadFile() {
+    const fileInput = this.$refs.fileInput;
+    if (fileInput.files.length > 0) {
+      this.selectedFile = fileInput.files[0];
+      this.selectedFileName = fileInput.files[0].name;
+    console.log(this.selectedFile)
+    }
+  },
     Submitfn() {
-      let payload = {
+      if (this.selectedFile) {
+       
+
+       const formData = new FormData();
+       formData.append('file', this.selectedFile);
+
+       let payload = {
         data: {
           name: this.customerName,
           email: this.customerEmail,
           position: this.PositionOptions,
           linkedIn_profile: this.customerLinkedin,
-          upload_resume: this.customerResume
+          upload_resume: formData
         }
 
       }
@@ -110,7 +138,12 @@ export default {
       } catch (error) {
         console.error("Error fetching data:", error);
       }
+     } else {
+       // Handle the case where no file is selected or an error occurred.
+     }
+    
     },
+    
     async fetchCareerPositions() {
 
       try {
@@ -119,7 +152,7 @@ export default {
         this.cardData = this.cards.map(card => ({
           title: card.attributes.job_title,
           qualifications: card.attributes.qualifications.data.map(description => description.attributes.qualification),
-          link:card.attributes.link
+          link: card.attributes.link
         }));
         this.options = [];
         for (let i = 0; i < this.cardData.length; i++) {
@@ -130,15 +163,40 @@ export default {
       }
 
     },
+
   }
 }
 </script>
 <style scoped>
-.bg-green-and-black {
+/* .bg-green-and-black {
   background: linear-gradient(105deg, rgb(56, 150, 162) 50%, #CCEAE6 50%);
-  /* Other styling properties for the div */
-}
+  position: relative;
+  background-size: cover, cover;
+  z-index: 1;
+} */
 
+.bg-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('/assets/images/carreerBg.png'); 
+  background-size: cover;
+  background-repeat: no-repeat;
+  z-index: 0; 
+  
+}
+.bg-image::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(105deg, rgb(56, 150, 162,0.5) 50%, rgba(204, 234, 230) 50%);/* Set your desired color and opacity */
+  z-index: 2; /* Ensure the overlay is above the image */
+}
 @media (max-width: 640px) {
   .bg-green-and-black {
     background: #CCEAE6;
