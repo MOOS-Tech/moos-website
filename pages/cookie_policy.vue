cd7aa0831f6f2c16be1cfde39c17d6530fe206a4
Dhanushka Sandaruwan <dhanushkasandaruwan350@gmail.com>
2023-11-10 14:27:45 +0530

    new: cookie,privacy,terms pages

---
+++ pages/cookie_policy.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="height: 80vh;padding-top: 50px;padding-left: 50px">cookie_policy</div>
+</template>
+
+<script>
+
+export default {
+  name: "cookie_policy",
+  components: {
+  },
+  computed: {
+
+  },
+  data() {
+    return {
+
+    };
+  }
+};
+</script>
+<style scoped>
+
+</style>
---
+++ pages/privacy_policy.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="height: 80vh;padding-top: 50px;padding-left: 50px">privacy_policy</div>
+</template>
+
+<script>
+
+export default {
+  name: "privacy_policy",
+  components: {
+  },
+  computed: {
+
+  },
+  data() {
+    return {
+
+    };
+  }
+};
+</script>
+<style scoped>
+
+</style>
